2e3a6fe73fa9208e83f8af562991b7baacc27160
HarryT182 <htho0024@student.monash.edu>
2024-10-15 23:48:22 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,9 @@
+var vg_1 = "PSM.json"; 
+vegaEmbed("#psm", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "Choropleth.json";
+vegaEmbed("#choropleth", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
